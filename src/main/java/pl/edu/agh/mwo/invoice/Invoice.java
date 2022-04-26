379620ec406b7686869da1a88826fa31956c2449
Jakub Pikula <j.pikula97@gmail.com>
2022-04-26 22:50:34 +0200

    second commot

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,71 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
-import java.util.Collection;
-
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private int number;
+    static int nextNumber = 1;
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+
+    public Invoice() {
+        this.number = nextNumber++;
+    }
 
     public void addProduct(Product product) {
-        // TODO: implement
+        addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (product == null || quantity <= 0) {
+            throw new IllegalArgumentException();
+        }
+        products.put(product, products.getOrDefault(product, 0) + quantity);
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetTotal() {
+        BigDecimal totalNet = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalNet = totalNet.add(product.getPrice().multiply(quantity));
+        }
+        return totalNet;
     }
 
-    public BigDecimal getTax() {
-        return null;
+    public BigDecimal getTaxTotal() {
+        return getGrossTotal().subtract(getNetTotal());
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossTotal() {
+        BigDecimal totalGross = BigDecimal.ZERO;
+        for (Product product : products.keySet()) {
+            BigDecimal quantity = new BigDecimal(products.get(product));
+            totalGross = totalGross.add(product.getPriceWithTax().multiply(quantity));
+        }
+        return totalGross;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getItems() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(this.getNumber());
+        stringBuilder.append('\n');
+        for (Product product : products.keySet()) {
+            stringBuilder.append(product.getName() + " "
+                    + products.get(product) + " " + product.getPrice() + "\n");
+        }
+
+        stringBuilder.append("Liczba pozycji: " + String.valueOf(products.size()));
+        return stringBuilder.toString();
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return this.products;
     }
 }
--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -1,33 +1,59 @@
 package pl.edu.agh.mwo.invoice.product;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public abstract class Product {
     private final String name;
 
-    private final BigDecimal price;
+    protected final BigDecimal price;
 
-    private final BigDecimal taxPercent;
+    protected final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
+
+        if (name == null || name.equals("") || price == null || tax == null
+                || tax.compareTo(new BigDecimal(0)) < 0
+                || price.compareTo(new BigDecimal(0)) < 0) {
+            throw new IllegalArgumentException();
+        }
         this.name = name;
         this.price = price;
         this.taxPercent = tax;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return price.multiply(taxPercent).add(price);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) && Objects.equals(price, product.price)
+                && Objects.equals(taxPercent, product.taxPercent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, taxPercent);
+    }
+
 }
